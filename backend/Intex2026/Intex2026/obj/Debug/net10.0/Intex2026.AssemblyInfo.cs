b6f1a1c42932ebddd7aa479eb071f52ed402c584
Aceluke24 <aceluke24@gmail.com>
2026-04-06 15:00:36 -0600

    Cleaned some things up

--- backend/Intex2026/Intex2026/obj/Debug/net10.0/Intex2026.AssemblyInfo.cs
+++ backend/Intex2026/Intex2026/obj/Debug/net10.0/Intex2026.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Intex2026")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30287e5a6a03649323d17e6365702f0669884e3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Intex2026")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Intex2026")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
