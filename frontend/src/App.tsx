450c587d14b0a92381bbc7612f779723e6101854
Aceluke24 <aceluke24@gmail.com>
2026-04-06 14:23:08 -0600

    Added .gitignore file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,8 @@
-import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 
 function App() {
-  
+
   return (
     <>
     <Router>
