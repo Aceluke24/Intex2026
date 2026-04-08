c0a478704df2a34d947048f2bc952c810498b1eb
Aceluke24 <aceluke24@gmail.com>
2026-04-07 22:00:03 -0600

    Deploy problems

---
+++ backend/Intex2026/Intex2026/Migrations/AppDb/20260408035517_SyncAppDbModelForProdBoot.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Intex2026.Migrations.AppDb
+{
+    /// <inheritdoc />
+    public partial class SyncAppDbModelForProdBoot : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
