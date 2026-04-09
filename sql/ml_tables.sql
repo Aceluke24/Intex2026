5720f5126b0ba998b0a3257e81ab868200ed0aef
Spencer Morgan <spencermorgan72701@gmail.com>
2026-04-09 15:28:16 -0600

    change to ml_tables.sql script

--- sql/ml_tables.sql
+++ sql/ml_tables.sql
@@ -1,9 +1,12 @@
-CREATE TABLE ml_donor_scores (
+CREATE SCHEMA ml;
+GO
+
+CREATE TABLE ml.donor_scores (
     supporter_id INT NOT NULL,
     donation_id INT NOT NULL,
     repeat_probability_180d FLOAT NOT NULL,
     scored_at DATETIME2 NOT NULL,
     model_version NVARCHAR(50) NOT NULL,
 
-    CONSTRAINT PK_ml_donor_scores PRIMARY KEY (supporter_id)
+    CONSTRAINT PK_donor_scores PRIMARY KEY (supporter_id)
 );
